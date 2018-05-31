b3eb3ec1b13409c2f6311e0aeb1d4e9b65438aee
Praqma Release User <man@praqma.net>
2018-05-31 12:43:25 +0200

    Master

--- vars/recordLabels.groovy
+++ vars/recordLabels.groovy
@@ -1,11 +1,13 @@
 def list(String nodeName = "master") {
     echo "Using nodeName as: '$nodeName'"
     echo "Jenkins instance is not null: ${Jenkins.instance != null}"
+    if(nodeName == "master") {
+        return "master"
+    }
     def currentNode = Jenkins.instance.getNode(nodeName)
     echo "Found current node: $currentNode"
     def labelAtoms = currentNode.getAssignedLabels()
     for(labelName in labelAtoms) {
         echo "$labelName"
     }
-    return "hehe"
 }
